3be8d1d2fcff44a1d5b1cc84dae5775af2d1cb2b
Andrii Polunin <apolunin@gmail.com>
2015-05-29 14:57:33 +0300

    implement 'cube' function

--- feature1.c
+++ feature1.c
@@ -16,3 +16,7 @@ int mul(int x, int y) {
     return x * y;
 }
 
+int cube(int x) {
+    return x * x * x;
+}
+
--- feature1.h
+++ feature1.h
@@ -7,5 +7,7 @@ int sub(int x, int y);
 int mul(int x, int y);
 int div(int x, int y);
 
+int cube(int x);
+
 #endif
 
