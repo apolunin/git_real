af6b2abb2e1925928b5c14b12468f40a97d6128d
Andrii Polunin <apolunin@gmail.com>
2015-05-29 14:13:32 +0300

    reorder functions declarations/definitions

--- feature1.c
+++ feature1.c
@@ -1,15 +1,15 @@
 #include "feature1.h"
 
-int div(int x, int y) {
-    return x / y;
-}
-
 int add(int x, int y) {
     return x + y;
 }
 
 int sub(int x, int y) {
     return x - y;
+}
+
+int div(int x, int y) {
+    return x / y;
 }
 
 int mul(int x, int y) {
--- feature1.h
+++ feature1.h
@@ -1,10 +1,11 @@
 #ifndef __FEATURE1_H__
 #define __FEATURE1_H__
 
-int div(int x, int y);
 int add(int x, int y);
 int sub(int x, int y);
+
 int mul(int x, int y);
+int div(int x, int y);
 
 #endif
 
